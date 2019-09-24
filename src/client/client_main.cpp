a5fb01337192367269589ecd37534f8c7d0e00fb
AaronRobert <Robert.Aaron2017@outlook.com>
2019-09-24 11:08:09 +0800

    add google style code

--- src/client/client_main.cpp
+++ src/client/client_main.cpp
@@ -1,34 +1,33 @@
-#include <sys/types.h>
-#include <sys/socket.h>
+/**
+ * Copyright 2019 Aaron Robert
+ * */
 #include <netinet/in.h>
-#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <iostream>
 #include <sstream>
+#include <string>
 const size_t MAXSIZE = 4096;
-using namespace std;
-int main()
-{
-    int totalPacket = 0, ackPacket = 0;
-    int iSockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    sockaddr_in *sSvrAddr = new sockaddr_in;
-    sSvrAddr->sin_family = AF_INET;
-    sSvrAddr->sin_addr.s_addr = htonl(INADDR_ANY);
-    sSvrAddr->sin_port = htons(9877);
-    int n;
-    char data[] = {0x01, 0x02};
-    char data2[65500];
-    for (int i = 0; i < 10000; i++)
-    {
-        totalPacket++;
-        n = sendto(iSockfd, data2, sizeof(data2), 0, (sockaddr *)sSvrAddr, sizeof(*sSvrAddr));
-        if (n == -1)
-            continue;
-        char ackData[1];
-        n = recvfrom(iSockfd, ackData, sizeof(ackData), 0, NULL, NULL);
-        if (n == -1)
-            continue;
-        if (ackData[0] == 0x01)
-            ackPacket++;
-        cout << "ACK/TOTAL: " << ackPacket << "/" << totalPacket << " " << ackPacket * 1.0 / totalPacket << endl;
-    }
+int main() {
+  int totalPacket = 0, ackPacket = 0;
+  int iSockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  sockaddr_in *sSvrAddr = new sockaddr_in;
+  sSvrAddr->sin_family = AF_INET;
+  sSvrAddr->sin_addr.s_addr = htonl(INADDR_ANY);
+  sSvrAddr->sin_port = htons(9877);
+  int n;
+  char data[] = {0x01, 0x02};
+  char data2[65500];
+  for (int i = 0; i < 10000; i++) {
+    totalPacket++;
+    n = sendto(iSockfd, data2, sizeof(data2), 0,
+               reinterpret_cast<sockaddr *>(sSvrAddr), sizeof(*sSvrAddr));
+    if (n == -1) continue;
+    char ackData[1];
+    n = recvfrom(iSockfd, ackData, sizeof(ackData), 0, NULL, NULL);
+    if (n == -1) continue;
+    if (ackData[0] == 0x01) ackPacket++;
+    std::cout << "ACK/TOTAL: " << ackPacket << "/" << totalPacket << " "
+              << ackPacket * 1.0 / totalPacket << std::endl;
+  }
 }
--- src/server/server_main.cpp
+++ src/server/server_main.cpp
@@ -1,50 +1,49 @@
-#include <sys/types.h>
-#include <sys/socket.h>
+/**
+ * Copyright 2019 Aaron Robert
+ * */
+#include <errno.h>
 #include <netinet/in.h>
-#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <iostream>
 #include <sstream>
-#include <errno.h>
+#include <string>
 
 const size_t MAXSIZE = 65535;
-char const hex_chars[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-std::string DataToHex(char *data, size_t len)
-{
-    std::stringstream ss;
-    for (int i = 0; i < len; i++)
-    {
-        ss << hex_chars[(data[i] & 0xF0) >> 4];
-        ss << hex_chars[(data[i] & 0x0F) >> 0];
-        ss << ' ';
-    }
-    return ss.str();
+char const hex_chars[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+std::string DataToHex(char *data, size_t len) {
+  std::stringstream ss;
+  for (int i = 0; i < len; i++) {
+    ss << hex_chars[(data[i] & 0xF0) >> 4];
+    ss << hex_chars[(data[i] & 0x0F) >> 0];
+    ss << ' ';
+  }
+  return ss.str();
 }
-using namespace std;
-int main()
-{
-    int iRet;
-    int iSockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (iSockfd == -1)
-        cout << "socket error:" << errno << endl;
-    sockaddr_in *sSvrAddr = new sockaddr_in;
-    sockaddr_in *sCliAddr = new sockaddr_in;
-    sSvrAddr->sin_family = AF_INET;
-    sSvrAddr->sin_addr.s_addr = htonl(INADDR_ANY);
-    sSvrAddr->sin_port = htons(9877);
-    iRet = bind(iSockfd, (sockaddr *)sSvrAddr, sizeof(*sSvrAddr));
-    if (iRet == -1)
-        cout << "bind error:" << errno << endl;
-    while (1)
-    {
-        int n;
-        socklen_t len = sizeof(*sCliAddr);
-        char data[MAXSIZE];
-        n = recvfrom(iSockfd, data, MAXSIZE, 0, (sockaddr *)sCliAddr, &len);
-        if (n == -1)
-            cout << "recvfrom error:" << errno << endl;
-        cout << "Recv[" << n << "] " << endl;
-        //cout << DataToHex(data, n) << endl;
-        char ackData[] = {0x01};
-        iRet = sendto(iSockfd, ackData, sizeof(ackData), 0, (sockaddr *)sCliAddr, len);
-    }
+int main() {
+  int iRet;
+  int iSockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (iSockfd == -1) std::cout << "socket error:" << errno << std::endl;
+  sockaddr_in *sSvrAddr = new sockaddr_in;
+  sockaddr_in *sCliAddr = new sockaddr_in;
+  sSvrAddr->sin_family = AF_INET;
+  sSvrAddr->sin_addr.s_addr = htonl(INADDR_ANY);
+  sSvrAddr->sin_port = htons(9877);
+  iRet =
+      bind(iSockfd, reinterpret_cast<sockaddr *>(sSvrAddr), sizeof(*sSvrAddr));
+  if (iRet == -1) std::cout << "bind error:" << errno << std::endl;
+  while (1) {
+    int n;
+    socklen_t len = sizeof(*sCliAddr);
+    char data[MAXSIZE];
+    n = recvfrom(iSockfd, data, MAXSIZE, 0,
+                 reinterpret_cast<sockaddr *>(sCliAddr), &len);
+    if (n == -1) std::cout << "recvfrom error:" << errno << std::endl;
+    std::cout << "Recv[" << n << "] " << std::endl;
+    // cout << DataToHex(data, n) << endl;
+    char ackData[] = {0x01};
+    iRet = sendto(iSockfd, ackData, sizeof(ackData), 0,
+                  reinterpret_cast<sockaddr *>(sCliAddr), len);
+  }
 }
