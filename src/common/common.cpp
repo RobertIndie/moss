903e799a9461f92558db9fd53f5ac32962268f6c
AaronRobert <Robert.Aaron2017@outlook.com>
2019-09-29 16:40:33 +0800

    add channel

--- src/common/common.cpp
+++ src/common/common.cpp
@@ -1,5 +1,14 @@
 /**
  * Copyright 2019 Aaron Robert
  * */
-#include "common.h"
+#include "common/common.h"
 
+int UDPChannel::Connect(std::string ip, unsigned short port) {
+  this->socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  this->sa->sin_family = AF_INET;
+  // store this IP address in sa:
+  inet_pton(AF_INET, ip.c_str(), &(this->sa->sin_addr));
+  this->sa->sin_port = htons(port);
+}
+
+Data* UDPChannel::Send(Data* data) {}
--- src/common/common.h
+++ src/common/common.h
@@ -1,7 +1,17 @@
 /**
  * Copyright 2019 Aaron Robert
  * */
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 #include <cstdint>
+#include <string>
+
+struct Data {
+  char *data;
+  size_t len;
+};
+
 template <typename TimeType = int32_t, typename CountType = int8_t>
 class RTTInfo {
  public:
@@ -11,4 +21,22 @@ class RTTInfo {
   TimeType rto;
   CountType retransmitted_count;
   uint32_t time_base;
+};
+
+class Channel {
+ public:
+  virtual int Connect(std::string ip, unsigned short port) = 0;
+  virtual Data *Send(Data *data) = 0;
+};
+
+class UDPChannel : public Channel {
+ public:
+  UDPChannel() { this->sa = new sockaddr_in; }
+  int Connect(std::string ip, unsigned short port);
+  Data *Send(Data *data);
+
+ private:
+  int socket_fd;
+  sockaddr_in *sa;
+  uint32_t seq;
 };
