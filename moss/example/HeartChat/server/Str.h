6769f670857ffbc73831676e48c2c5d81a7db97f
moss <yaofunvtion@163.com>
2019-10-12 23:41:00 +0800

    DeleleInvailedFiles

--- moss/example/HeartChat/server/Str.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <vector>
-#include <string>
-
-std::vector<std::string*>  SpiltStr(const std::string& TotalStr, const char& spiltChar);
-bool MatchStr(const std::string& Match_Str, const std::string& Source_Str);
-int SerchID(const std::vector<std::string> str, const std::string& ID);
