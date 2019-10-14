065ef99a395f10bb1bf541015db9cc0484344aa8
AaronRobert <Robert.Aaron2017@outlook.com>
2019-10-14 10:46:45 +0800

    up

--- benchmark/client/client.cc
+++ benchmark/client/client.cc
@@ -7,40 +7,47 @@
 #include "fstream"
 #include "rpc/rpc.h"
 
-void proc(std::fstream &fs, const char *ip, int port) {
+void proc(int id, std::fstream &fs_err, std::fstream &fs_result, const char *ip,
+          int port) {
   UDPClientChannel channel;
   channel.Connect(ip, port);
   ClientProxy prx(&channel);
-  benchmark_test::LoginMessage req;
-  benchmark_test::LoginResult res;
-  req.set_id(12138);
-  req.set_password("123456");
-  prx.Call("Login", &req, &res);
-  if (res.code() == 0 && res.error() == "No Error") {
-    fs << "ERROR" << std::endl;
+  while (1) {
+    benchmark_test::LoginMessage req;
+    benchmark_test::LoginResult res;
+    req.set_id(12138);
+    req.set_password("123456");
+    prx.Call("Login", &req, &res);
+    if (res.code() == 0 && res.error() == "No Error") {
+      fs_result << id << ":Sent" << std::endl;
+    } else {
+      fs_err << id << ":ERROR" << std::endl;
+    }
   }
 }
 
 int main(int argc, char *argv[]) {
-  std::fstream fs("./output.txt");
-  if (!fs.is_open()) {
+  std::fstream fs_err("./error.txt");
+  std::fstream fs_result("./result.txt");
+  if (!fs_err.is_open()) {
     printf("output.txt dose not exist!\n");
+    return -1;
+  }
+  if (!fs_result.is_open()) {
+    printf("result.txt does not exist!\n");
     return -1;
   }
   const char *ip = argv[1];
   int port = atoi(argv[2]);
   int task = atoi(argv[3]);
   int proccnt = atoi(argv[4]);
-
-  fs << 1;
-  fs.close();
-  return 0;
   if (argc < 5) {
     printf(
         "Usage:\n"
         "example_echosvr [IP] [PORT] [TASK] [PROCESS_COUNT]\n");
     return -1;
   }
+
   std::vector<pid_t> pids;
   bool isParent = false;
   for (int i = 0; i < proccnt; i++) {
@@ -51,10 +58,13 @@ int main(int argc, char *argv[]) {
     } else if (pid < 0) {
       printf("Create proc error!\n");
     }
-    proc(fs, ip, port);
+    proc(i, fs_err, fs_result, ip, port);
   }
   if (isParent) {
-      
+    sleep(10);
+    for (std::vector<pid_t>::iterator iter; iter != pids.end(); iter++) {
+      kill(*iter, SIGABRT);
+    }
   }
   return 0;
 }
--- benchmark/server/server.cc
+++ benchmark/server/server.cc
@@ -1,1 +1,42 @@
-int main() { return 0; }
+/**
+ * Copyright 2019 Aaron Robert
+ * */
+#include "./login.pb.h"
+#include "rpc/rpc.h"
+
+Data *handle(void *context, Data *const request) {
+  Data *response = new Data(1);
+  return response;
+}
+
+int Login(std::stringstream *in_data, std::stringstream *out_data) {
+  benchmark_test::LoginMessage req;
+  benchmark_test::LoginResult res;
+  ConvertStreamToProtoObj(&req, in_data);
+  if (req.id() == 12138 && req.password() == "123456") {
+    res.set_code(0);
+    res.set_error("No Error");
+  } else {
+    res.set_code(-1);
+    res.set_error("Error");
+  }
+  ConvertProtoObjToStream(&res, out_data);
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  const char *ip = argv[1];
+  int port = atoi(argv[2]);
+  if (argc < 3) {
+    printf(
+        "Usage:\n"
+        "example_echosvr [IP] [PORT]\n");
+    return -1;
+  }
+  UDPServerChannel channel;
+  channel.Bind(ip, port);
+  ServerProxy prx(&channel);
+  REG_FUNC(prx, Login);
+  prx.Serve();
+  return 0;
+}
