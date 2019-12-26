8e36c9d33859cb187cedab82a413177696245c16
Aaron Robert <Robert.Aaron2017@outlook.com>
2019-12-26 21:22:44 +0800

    up

--- test/moss_test/test_databuffer.cc
+++ test/moss_test/test_databuffer.cc
@@ -24,6 +24,10 @@ TEST(DataBuffer, DPTR) {
   auto a = buffer.NewReader();
   auto b = buffer.NewReader();
   buffer.writer_pos_ = 5;
+  // 测试MovePtr
+  EXPECT_EQ(a->Move(a->ptr_, -1), 7);
+  EXPECT_EQ(a->Move(a->ptr_, 7), 7);
+  EXPECT_EQ(a->Move(a->ptr_, 8), 0);
   *a += 1;
   EXPECT_EQ(a->ptr_, 1);
   EXPECT_EQ(*a > *b, true);
